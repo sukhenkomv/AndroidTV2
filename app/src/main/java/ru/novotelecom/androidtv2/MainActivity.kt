fd12b597a9cad7c76ed57d2a82d3f503a23985fd
Maksim Sukhenko <suhenko@mail.ru>
2022-03-18 10:02:49 +0700

    Список полностью

--- app/src/main/java/ru/novotelecom/androidtv2/MainActivity.kt
+++ app/src/main/java/ru/novotelecom/androidtv2/MainActivity.kt
@@ -2,18 +2,39 @@ package ru.novotelecom.androidtv2
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.KeyEvent
+import android.view.View
+import android.widget.FrameLayout
+import androidx.core.view.isVisible
 import androidx.leanback.widget.ArrayObjectAdapter
-import androidx.leanback.widget.VerticalGridPresenter
+import androidx.leanback.widget.Visibility
 
 class MainActivity : AppCompatActivity() {
 
     lateinit var gridView: MyGridView
+    lateinit var gridViewWrapper: FrameLayout
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        gridView = supportFragmentManager.findFragmentById(R.id.myGridViewID) as MyGridView
-//        gridView.title = "Title"
+        gridViewWrapper = findViewById(R.id.gridViewWrapper)
+
+        gridView = supportFragmentManager.findFragmentById(R.id.myGridView) as MyGridView
+
+        gridView.setSelectedPosition(0)
+
+        gridView.setOnItemViewClickedListener { itemViewHolder, item, rowViewHolder, row ->
+            Log.i("OnItemViewClicked", item as String)
+        }
+
+        gridView.setOnItemViewSelectedListener { itemViewHolder, item, rowViewHolder, row ->
+            Log.i("OnItemViewSelected", item as String)
+        }
+
+
 
         val gridPresenter = MyGridPresenter()
         gridPresenter.numberOfColumns = 1
@@ -33,4 +54,20 @@ class MainActivity : AppCompatActivity() {
         gridView.adapter = adapter
     }
 
+    override fun onKeyDown(keyCode: Int, event: KeyEvent?): Boolean {
+        when (keyCode) {
+            KeyEvent.KEYCODE_DPAD_LEFT -> {
+                if (gridViewWrapper.isVisible) {
+                    gridViewWrapper.visibility = View.INVISIBLE
+                    return true
+                }
+                if (!gridViewWrapper.isVisible) {
+                    gridViewWrapper.visibility = View.VISIBLE
+                    return true
+                }
+            }
+        }
+
+        return super.onKeyDown(keyCode, event)
+    }
 }
--- app/src/main/java/ru/novotelecom/androidtv2/MyItemPresenter.kt
+++ app/src/main/java/ru/novotelecom/androidtv2/MyItemPresenter.kt
@@ -7,7 +7,6 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
 import android.widget.TextView
-import androidx.leanback.widget.BaseCardView
 import androidx.leanback.widget.Presenter
 
 
@@ -16,35 +15,15 @@ class MyItemPresenter: Presenter() {
     override fun onCreateViewHolder(parent: ViewGroup): ViewHolder {
         val view: View = LayoutInflater.from(parent.context)
             .inflate(R.layout.list_item_layout, parent, false)
-//        return ViewHolder(ItemHolder(parent.context, view))
-//        return ItemHolder(view)
         return ItemHolder(view)
-
-//        return ViewHolder(TextView(parent.context))
     }
 
     override fun onBindViewHolder(viewHolder: ViewHolder, item: Any?) {
-//        val view = viewHolder.view as ItemHolder
         (viewHolder as ItemHolder).bind(item)
-//        val view = viewHolder.view as TextView
-//        view.text = item as String
     }
 
     override fun onUnbindViewHolder(viewHolder: ViewHolder?) {}
 }
-
-//class ItemHolder (context: Context, itemView: View?): BaseCardView(context) {
-//    private val image: ImageView = itemView!!.findViewById(R.id.listItemImageID)
-//    private val title: TextView = itemView!!.findViewById(R.id.listItemTextID)
-//
-//    constructor(context: Context) : this(context, null)
-//
-//    fun bind(
-//        item: Any?
-//    ) {
-//        title.text = item as String
-//    }
-//}
 
 class ItemHolder (itemView: View): Presenter.ViewHolder(itemView) {
     private val image: ImageView = itemView.findViewById(R.id.listItemImageID)
